a4f009c421cd21d687fc9ef82917511fef46dc97
Cisco <franciscoraglandjr@gmail.com>
2015-12-15 17:34:46 -0800

    added ChoreDetailVC

---
+++ PeaceKeeper/ChoreDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChoreDetailViewController.h
+//  PeaceKeeper
+//
+//  Created by Francisco Ragland Jr on 12/15/15.
+//  Copyright © 2015 Francisco Ragland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChoreDetailViewController : UIViewController
+
+@end
