906c3d2394563b805a00f775e5b631d6a40327d2
HoodsDream <idinkyy@gmail.com>
2015-12-17 19:51:15 -0800

    added default image / changed nav bar tint white

--- PeaceKeeper/AppDelegate.m
+++ PeaceKeeper/AppDelegate.m
@@ -26,7 +26,6 @@
 }
 
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
